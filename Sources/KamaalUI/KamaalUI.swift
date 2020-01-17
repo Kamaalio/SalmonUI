6df1c34d7fdc900d2f8f1c234928bffb7da478ac
Kamaal Farah <kamaal.f1@gmail.com>
2020-01-17 21:59:05 +0100

    Creating first view

--- Sources/KamaalUI/KamaalUI.swift
+++ Sources/KamaalUI/KamaalUI.swift
@@ -1,7 +1,32 @@
+import SwiftUI
 
 #if canImport(SwiftUI)
 public struct Hello {
     public var text = "Hello, World!"
     public init() { }
+}
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public struct KTextField: View {
+    var value: Binding<String>
+    let name: String
+    let placeholder: String
+    let disableTextField: Bool
+
+    public init(value: Binding<String>, name: String, placeholder: String, disableTextField: Bool) {
+        self.value = value
+        self.name = name
+        self.placeholder = placeholder
+        self.disableTextField = disableTextField
+    }
+
+    public var body: some View {
+        HStack {
+            Text(name)
+                .frame(minWidth: 75 , maxWidth: 75, minHeight: 40, maxHeight: 40)
+            TextField(placeholder, text: value)
+                .disabled(disableTextField)
+        }
+    }
 }
 #endif
