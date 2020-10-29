9f20f5714a3697ebbd7d6efff8c6c380fa1576e3
Kamaal Farah <kamaal.f1@gmail.com>
2020-10-29 08:00:16 +0100

    The great rename

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "KamaalUI",
+    name: "SalmonUI",
     platforms: [
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
     ],
     products: [
-        .library(name: "KamaalUI", targets: ["KamaalUI"]),
+        .library(name: "SalmonUI", targets: ["SalmonUI"]),
     ],
     dependencies: [],
     targets: [
-        .target(name: "KamaalUI", dependencies: []),
-        .testTarget(name: "KamaalUITests", dependencies: ["KamaalUI"]),
+        .target(name: "SalmonUI", dependencies: []),
+        .testTarget(name: "SalmonUITests", dependencies: ["SalmonUI"]),
     ]
 )
--- Tests/SalmonUITests/XCTestManifests.swift
+++ Tests/SalmonUITests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(KamaalUITests.allTests),
+        testCase(SalmonUITests.allTests),
     ]
 }
 #endif
