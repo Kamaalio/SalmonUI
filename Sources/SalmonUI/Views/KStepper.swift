4d1d4a3dcbdee47b6f40a343ff0499a46e2c8456
kamaal111 <kamaal.f1@gmail.com>
2020-12-07 08:41:16 +0100

    Making stepper easier to work with

--- Sources/SalmonUI/Views/KStepper.swift
+++ Sources/SalmonUI/Views/KStepper.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 
-#if !os(macOS) && !os(tvOS)
+#if !os(tvOS)
 public struct KStepper: View {
+    #if !os(macOS) || !targetEnvironment(macCatalyst)
     @State private var incrementOpacity = 1.0
     @State private var decrementOpacity = 1.0
+    #endif
 
     public let size: CGSize
     public let disableIncrement: Bool
@@ -30,6 +32,7 @@ public struct KStepper: View {
         self.onDecrement = onDecrement
     }
 
+    #if !os(macOS) || !targetEnvironment(macCatalyst)
     public var body: some View {
         HStack {
             Image(systemName: "minus.rectangle.fill")
@@ -37,10 +40,12 @@ public struct KStepper: View {
                 .opacity(decrementOpacity)
                 .disabled(disableDecrement)
                 .onTapGesture(perform: {
-                    self.decrementOpacity = 0.2
-                    self.onDecrement()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        withAnimation { self.decrementOpacity = 1 }
+                    if !disableDecrement {
+                        self.decrementOpacity = 0.2
+                        self.onDecrement()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            withAnimation { self.decrementOpacity = 1 }
+                        }
                     }
                 })
             Image(systemName: "plus.rectangle.fill")
@@ -48,14 +53,34 @@ public struct KStepper: View {
                 .opacity(incrementOpacity)
                 .disabled(disableIncrement)
                 .onTapGesture(perform: {
-                    self.incrementOpacity = 0.2
-                    self.onIncrement()
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        withAnimation { self.incrementOpacity = 1 }
+                    if !disableIncrement {
+                        self.incrementOpacity = 0.2
+                        self.onIncrement()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            withAnimation { self.incrementOpacity = 1 }
+                        }
                     }
                 })
         }
         .foregroundColor(.accentColor)
     }
+    #else
+    public var body: some View {
+        HStack {
+            Button(action: onDecrement) {
+                Image(systemName: "minus.rectangle.fill")
+                    .size(size)
+                    .foregroundColor(.accentColor)
+            }
+            .disabled(disableDecrement)
+            Button(action: onIncrement) {
+                Image(systemName: "plus.rectangle.fill")
+                    .size(size)
+                    .foregroundColor(.accentColor)
+            }
+            .disabled(disableIncrement)
+        }
+    }
+    #endif
 }
 #endif
